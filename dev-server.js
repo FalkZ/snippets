dc8c824a0cb6362cfa0b9a4d72956c3c75ff9273
justaweb.dev <3829508-justaweb.dev@users.noreply.gitlab.com>
2020-10-12 15:23:57 +0200

    added new dev index

--- dev-server.js
+++ dev-server.js
@@ -1,16 +1,21 @@
-var liveServer = require("live-server");
-const { writeIndex } = require("create-index");
+const liveServer = require("live-server");
 const fs = require("fs");
+const { exec } = require("child_process");
 
 var params = {
   port: 12345,
   host: "localhost",
   open: false,
+  cors: true,
 };
 
 fs.watch(".", (type, file) => {
   console.log(type, file);
-  if (file !== "index.js") writeIndex(["."]);
+  if (type != "change" && file !== "index.js")
+    exec("npm run build", (err, val) => {
+      if (err) console.error(err);
+      else console.log(val);
+    });
 });
 
 liveServer.start(params);
